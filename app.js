555900d9208d90c89b6e7ecd97711adbc6ea1eb5
KatasonovYP <katasonov-yury@yandex.ru>
2021-11-08 03:26:27 +0300

    horizontal move

--- app.js
+++ app.js
@@ -8,11 +8,17 @@ document.addEventListener('keyup', game)
 
 function game(event) {
   switch (event.key) {
-    case 'ArrowDown':
-      down()
-      break
     case 'ArrowUp':
-      up()
+      verticalMove('up')
+      break
+    case 'ArrowDown':
+      verticalMove('down')
+      break
+    case 'ArrowLeft':
+      horizontalMove('left')
+      break
+    case 'ArrowRight':
+      horizontalMove('right')
       break
   }
 }
@@ -40,52 +46,71 @@ function getActivePositions(binField) {
   return activePositions
 }
 
-function down() {
+function verticalMove(direction) {
   let binField = getField()
-  const activePositions = getActivePositions(binField).reverse()
-  for (position of activePositions) {
-    const y = position[0]
-    const x = position[1]
-    let i = 1
-    if (y !== SIZE_OF_FIELD - i || binField[y + 1][x]) {
-      while (!binField[SIZE_OF_FIELD - i][x]) {
-        i++ // TODO может быть бесконечный цикл.
-      }
-      const active = rows[y].querySelectorAll('.cell')[x].querySelector('.active')
-      active.classList.add('move-right')
-      active.style.transform = `translate(0, ${(SIZE_OF_FIELD - i - y) * 120}px)`
-      setTimeout(() => repl(SIZE_OF_FIELD - i, active), 300)
-      binField[y][x] = true
-      binField[SIZE_OF_FIELD - i][x] = false
-    }
+  let activePositions = getActivePositions(binField)
+  let step, startValue
+  if (direction === 'up') {
+    startValue = 0
+    step = 1
+  } else if (direction === 'down') {
+    startValue = SIZE_OF_FIELD - 1
+    step = -1
+    activePositions = activePositions.reverse()
+  } else {
+    return
   }
-}
-
-function up() {
-  let binField = getField()
-  const activePositions = getActivePositions(binField)
   for (position of activePositions) {
     const y = position[0]
     const x = position[1]
-    let i = 0
-    if (y !== 0 || binField[y - 1][x]) {
+    let i = startValue
+    if (y !== startValue || !binField[y + step][x]) {
       while (!binField[i][x]) {
-        i++ // TODO может быть бесконечный цикл.
+         i += step // TODO может быть бесконечный цикл.
       }
       const active = rows[y].querySelectorAll('.cell')[x].querySelector('.active')
-      active.classList.add('move-right')
       active.style.transform = `translate(0, ${(i - y) * 120}px)`
-      setTimeout(() => repl(i, active), 300)
+      setTimeout(() => repl(i, active, x), 300)
       binField[y][x] = true
       binField[i][x] = false
     }
   }
 }
 
-function repl(position, active) {
-  active.classList.remove('move-right')
+function horizontalMove(direction) {
+  let binField = getField()
+  let activePositions = getActivePositions(binField)
+  let step, startValue
+  if (direction === 'left') {
+    startValue = 0
+    step = 1
+  } else if (direction === 'right') {
+    startValue = SIZE_OF_FIELD - 1
+    step = -1
+    activePositions = activePositions.reverse()
+  } else {
+    return
+  }
+  for (position of activePositions) {
+    const y = position[0]
+    const x = position[1]
+    let i = startValue
+    if (x !== startValue || !binField[y][x + step]) {
+      while (!binField[y][i]) {
+         i += step // TODO может быть бесконечный цикл.
+      }
+      const active = rows[y].querySelectorAll('.cell')[x].querySelector('.active')
+      active.style.transform = `translateX(${(i - x) * 120}px)`
+      setTimeout(() => repl(y, active, i), 300)
+      binField[y][x] = true
+      binField[y][i] = false
+    }
+  }
+}
+
+function repl(position, active, x) {
   active.style.transform = `translate(0, 0)`
-  rows[position].querySelector('.cell').append(active)
+  rows[position].querySelectorAll('.cell')[x].append(active)
 }
 
 function isEmpty(y, x) {
