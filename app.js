0b5c25c3cb0d44348041180803ef3a03a8418a9b
KatasonovYP <katasonov-yury@yandex.ru>
2021-11-10 01:02:16 +0300

    rename files

--- app.js
+++ app.js
@@ -1,131 +1,155 @@
 const SIZE_OF_FIELD = 4
 
+const colors = ['#EEE4DA', '#EDE0C8', '#F2B179', '#F59563', '#F67C5F', '#F65E3B', '#EDCF72', '#EDCC61']
+
 const rows = document.querySelectorAll('.row')
-const active = document.querySelector('.active')
-const field = document.querySelector('.field')
+const board = document.querySelector('.board')
 
-document.addEventListener('keyup', game)
+document.addEventListener('keyup', event => {
+  move(event.key)
+  createNewSquare()
+})
 
-function game(event) {
-  switch (event.key) {
-    case 'ArrowUp':
-      verticalMove('up')
-      break
-    case 'ArrowDown':
-      verticalMove('down')
-      break
-    case 'ArrowLeft':
-      horizontalMove('left')
-      break
-    case 'ArrowRight':
-      horizontalMove('right')
-      break
-  }
-  generateNewActive()
-}
+createNewSquare()
+createNewSquare()
 
-function getField() {
-  let binField = []
-  for (let i = 0; i < SIZE_OF_FIELD; i++) {
-    binField.push([])
-    for (let j = 0; j < SIZE_OF_FIELD; j++) {
-      binField[i].push(isEmpty(i, j))
-    }
-  }
-  return binField
-}
-
-function getActivePositions(binField) {
-  let activePositions = []
-  for (let i = 0; i < SIZE_OF_FIELD; i++) {
-    for (let j = 0; j < SIZE_OF_FIELD; j++) {
-      if (!binField[i][j]) {
-        activePositions.push([i, j])
-      }
-    }
-  }
-  return activePositions
-}
-
-function verticalMove(direction) {
-  let binField = getField()
-  let activePositions = getActivePositions(binField)
-  let step, startValue
-  switch (direction) {
-    case 'up':
-      startValue = 0
-      step = 1
-      break;
-    case 'down':
-      startValue = SIZE_OF_FIELD - 1
-      step = -1
-      activePositions = activePositions.reverse()
-      break;
-    default:
-      return;
-  }
-  for (position of activePositions) {
-    const y = position[0]
-    const x = position[1]
-    let i = startValue // может быть Y ом
-    if (y !== startValue || !binField[y + step][x]) {
-      while (!binField[i][x]) {
-         i += step // TODO может быть бесконечный цикл.
-      }
-      const active = rows[y].querySelectorAll('.cell')[x].querySelector('.active')
-      active.style.transform = `translateY(${(i - y) * 120}px)`
-      setTimeout(() => repl(i, active, x), 300)
-      binField[y][x] = true
-      binField[i][x] = false
-    }
-  }
-}
-
-function horizontalMove(direction) {
-  let binField = getField()
-  let activePositions = getActivePositions(binField)
-  let step, startValue
-  if (direction === 'left') {
-    startValue = 0
-    step = 1
-  } else if (direction === 'right') {
-    startValue = SIZE_OF_FIELD - 1
-    step = -1
-    activePositions = activePositions.reverse()
-  } else {
+function move(direction) {
+  if (!direction.includes('Arrow')) {
     return
   }
-  for (position of activePositions) {
-    const y = position[0]
-    const x = position[1]
-    let i = startValue
-    if (x !== startValue || !binField[y][x + step]) {
-      while (!binField[y][i]) {
-         i += step // TODO может быть бесконечный цикл.
+  let actives = getActives()
+  if (contains(['ArrowDown', 'ArrowRight'], direction)) {
+    actives = actives.reverse()
+  }
+  for (currentCell of actives) {
+    const emptyCells = getOtherEmptyCells(currentCell)
+    const cells = contains(['ArrowRight', 'ArrowLeft'], direction) ?
+                    currentCell.parentNode.querySelectorAll('.cell') : 
+                    getElementsFromCol(currentCell)
+    const index = findIndexByElem(cells, currentCell)
+    let newIndex = index
+    if (direction === 'ArrowRight' || direction === 'ArrowDown') {
+      for (let i = index; i < cells.length; i++) {
+        if (contains(emptyCells, cells[i])) {
+          newIndex = i
+        }
       }
-      const active = rows[y].querySelectorAll('.cell')[x].querySelector('.active')
-      active.style.transform = `translateX(${(i - x) * 120}px)`
-      setTimeout(() => repl(y, active, i), 300)
-      binField[y][x] = true
-      binField[y][i] = false
+    } else if (direction === 'ArrowLeft' || direction === 'ArrowUp') {
+      for (let i = index; i >= 0; i--) {
+        if (contains(emptyCells, cells[i])) {
+          newIndex = i
+        }
+      }
+    }
+    if (index !== newIndex) {
+      const active = currentCell.querySelector('.active')
+      const oldActive = cells[newIndex].querySelector('.active')
+      if (oldActive !== null) {
+        oldActive.parentNode.removeChild(oldActive)
+        const number = parseInt(active.innerHTML)
+        active.innerHTML = `${number * 2}`
+        const colorIndex = Math.round(Math.log(number) / Math.log(2))
+        if (colorIndex > 1) {
+          active.style.color = '#F9F6F2'
+        }
+        active.style.background = colors[colorIndex]
+      }
+      // animateMove(active, direction, index, newIndex)
+      replaceElement(cells[newIndex], active)
+      // Ошибка в том, что я сначала обрабатываю новый элемент, а затем уже перемещаю старый. чтобы исправить её, нужно собрать данные в массив и отдельно перемещать элементы из него
     }
   }
 }
 
-function generateNewActive() {
-  const binField = getField()
-  console.log(_.countBy(['foo', 'foo', 'bar', 'bon'])['foo'])
+function finishGame() {
+
 }
 
-function repl(position, active, x) {
-  active.style.transform = `translate(0, 0)`
-  rows[position].querySelectorAll('.cell')[x].append(active)
-}
-
-function isEmpty(y, x) {
-  if (x < 0 || x >= SIZE_OF_FIELD || y < 0 || y >= SIZE_OF_FIELD) {
-    return false
+function createNewSquare() {
+  const emptyCells = getEmptyCells()
+  if (emptyCells.length === 0) {
+    finishGame()
+  } else {
+    const index = getRandomNumber(0, emptyCells.length)
+    const square = document.createElement('div')
+    square.classList.add('active')
+    square.innerHTML = '2'
+    square.style.background = colors[0]
+    if (getRandomNumber(0, 10) == 0) {
+      square.style.background = colors[1]
+      square.innerHTML = '4'
+    }
+    emptyCells[index].append(square)
   }
-  const cell = rows[y].querySelectorAll('.cell')[x].querySelector('.active')
-  return cell === null // TODO contains
+}
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min) + min)
+}
+
+function getCell(rowID, colID) {
+  return rows[rowID].querySelectorAll('.cell')[colID]
+}
+
+function getAllCells() {
+  const allCells = []
+  for (row of rows) {
+    const cells = row.querySelectorAll('.cell')
+    for (cell of cells) {
+      allCells.push(cell)
+    }
+  }
+  return allCells
+}
+
+function getEmptyCells() {
+  const allCells = getAllCells()
+  return allCells.filter(cell => cell.querySelector('.active') === null)
+}
+
+function getOtherEmptyCells(element) {
+  const allCells = getAllCells()
+  return allCells.filter(cell => cell.querySelector('.active') === null ||
+    cell.querySelector('.active').innerHTML === element.querySelector('.active').innerHTML)
+}
+
+function getActives() {
+  const allCells = getAllCells()
+  return allCells.filter(cell => cell.querySelector('.active') !== null)
+}
+
+function findIndexByElem(nodeList, element) {
+  let index
+  nodeList.forEach((item, i) => item == element ? index = i : 0);
+  return index
+}
+
+function contains(arr, element) {
+  return arr.indexOf(element) !== -1
+}
+
+function getElementsFromCol(element) {
+  let elementsInCol = []
+  const elementsInRow = element.parentNode.querySelectorAll('.cell')
+  const index = findIndexByElem(elementsInRow, currentCell)
+  for (row of rows) {
+    const elementInCol = row.querySelectorAll('.cell')[index]
+    elementsInCol.push(elementInCol)
+  }
+  return elementsInCol
+}
+
+function animateMove(element, direction, start, end) {
+  let axis
+  if (contains(['ArrowUp', 'ArrowDown'], direction)) {
+    axis = 'Y'
+  } else {
+    axis = 'X'
+  }
+  element.style.transform = `translate${axis}(${(end - start) * 120}px)`
+}
+
+function replaceElement(newParent, element) {
+  element.style.transform = 'translateX(0)'
+  newParent.append(element)
 }
